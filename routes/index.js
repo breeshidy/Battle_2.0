40621f601a8fbd55bb1cf846f8d3ed778db12364
RyanVanDijck <rvandijck02@outlook.com>
2021-07-29 17:04:40 +0100

    Creating game class

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Player = require("../src/player.js");
+const Game = require("../src/game.js")
 
 let player = new Player();
 
@@ -13,16 +14,25 @@ router.post("/", (req, res) => {
 });
 
 router.post("/two_players", (req, res) => {
-  // console.log(req);
-  player1Name = req.body.player1Name;
-  player2Name = req.body.player2Name;
-  hpPlayer1 = player.health;
-  hpPlayer2 = player.health;
+  let player1
+  let player2
+  if(!req.app.locals.started){
+    player1 = new Player(req.body.player1Name);
+    player2 = new Player(req.body.player2Name);
+    req.app.locals.game = new Game(player1,player2)
+    req.app.locals.started = true
+  }
+  else{
+    req.app.locals.game.attack()
+    player1 = req.app.locals.game.player1
+    player2 = req.app.locals.game.player2
+  }
+
   res.render("game_start_2p.ejs", {
-    player1Name: player1Name,
-    player2Name: player2Name,
-    hpPlayer1: hpPlayer1,
-    hpPlayer2: hpPlayer2,
+    player1Name: player1.name,
+    player2Name: player2.name,
+    hpPlayer1: player1.health,
+    hpPlayer2: player2.health,
   });
 });
 
